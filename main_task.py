d6333305850338d5939f06e212d5df28744b9f92
chongzi1990 <yangchong.sdu@163.com>
2020-07-27 16:56:58 +0800

    1

---
+++ main_task.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from argparse import ArgumentParser
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', \
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logger = logging.getLogger('__file__')
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--task", type=str, default='classi', help='task type')
+    parser.add_argument("--train_data", type=str, default='./data/SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT', \
+                        help="training data .txt file path")
+
+    parser.add_argument("--train", type=int, default=1, help="0: Don't train, 1: train")
+    parser.add_argument("--infer", type=int, default=1, help="0: Don't infer, 1: Infer")
+
+    args = parser.parse_args()
+
+    if (args.train == 1) and (args.task != 'fewrel'):
+        net = train_and_fit(args)
+
+    if (args.infer == 1) and (args.task != 'fewrel'):
+        inferer = infer_from_trained(args, detect_entities=True)
+        test = "The surprise [E1]visit[/E1] caused a [E2]frenzy[/E2] on the already chaotic trading floor."
+        inferer.infer_sentence(test, detect_entities=False)
